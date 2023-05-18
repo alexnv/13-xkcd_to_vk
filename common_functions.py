246dc6b312edbe61dcd9ad7e989d56ab842bba5c
Непряхин Алексей <aleksey.nepryahin@outlook.com>
2023-05-18 12:51:18 +0300

    Реализован алгоритм скачивания комикса по id

---
+++ common_functions.py
@@ -0,0 +1,33 @@
+import os
+from pathlib import Path
+from urllib.parse import urlsplit, unquote
+
+import requests
+
+
+def save_image_to_file_from_url(image_url, file_name, payload=None):
+    response = requests.get(image_url, params=payload)
+    response.raise_for_status()
+
+    with open(file_name, 'wb') as file:
+        file.write(response.content)
+
+
+def get_file_extension_from_url(url):
+    url_structure = urlsplit(url)
+    path = unquote(url_structure.path)
+
+    head, tail = os.path.split(path)
+    root, ext = os.path.splitext(tail)
+
+    return ext
+
+
+def get_imagefolder_filename(filename):
+    "Функция возвращает путь к файлу в каталоге изображений. Путь не зависит от ОС"
+    return Path.cwd() / 'images' / f'{filename}'
+
+
+def get_imagefolder():
+    "Функция возвращает путь к каталогу изображений. Путь не зависит от ОС"
+    return Path.cwd() / 'images'
--- main.py
+++ main.py
@@ -1,2 +1,42 @@
+import logging
+import os
+
+import requests
+from dotenv import load_dotenv
+
+from common_functions import get_imagefolder, save_image_to_file_from_url, get_file_extension_from_url, \
+    get_imagefolder_filename
+
+
+def fetch_xkcd_comix(comixid = 0):
+    url = f"https://xkcd.com/info.0.json"
+    if comixid:
+        url = f"https://xkcd.com/{comixid}/info.0.json"
+
+    payload ={
+
+    }
+
+    response = requests.get(url, params=payload)
+    response.raise_for_status()
+
+    xkcd_comix_info = response.json()
+    if xkcd_comix_info['img']:
+        index = xkcd_comix_info['num']
+        image_extension = get_file_extension_from_url(xkcd_comix_info['img'])
+        file_name = get_imagefolder_filename(f'xkcd_{index}{image_extension}')
+        logging.info(f"Downloading image from url: {xkcd_comix_info['img']} to {file_name}")
+        save_image_to_file_from_url(xkcd_comix_info['img'], file_name)
+
+def setup():
+    logging.getLogger().setLevel(logging.INFO)
+    load_dotenv()
+    get_imagefolder().mkdir(parents=True, exist_ok=True)
+
+def main():
+    fetch_xkcd_comix(353)
+
+
 if __name__ == '__main__':
-    pass
+    setup()
+    main()
