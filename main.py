49a0a6ced915c67b2f94616e1c5da7d2a812f1bc
Непряхин Алексей <aleksey.nepryahin@outlook.com>
2023-05-30 09:24:48 +0300

    Выполнено улучшение читаемости кода

--- main.py
+++ main.py
@@ -8,14 +8,15 @@ import sys
 import requests
 from dotenv import load_dotenv
 
+from common_functions import save_image_to_file_from_url, \
+    get_file_extension_from_url
+
 VK_APIVERSION = "5.131"
 MAX_COMIX = 999
 
-from common_functions import save_image_to_file_from_url, get_file_extension_from_url
-
 
 def fetch_xkcd_comix(comixid=0):
-    url = f"https://xkcd.com/info.0.json"
+    url = "https://xkcd.com/info.0.json"
     if comixid:
         url = f"https://xkcd.com/{comixid}/info.0.json"
 
@@ -37,15 +38,18 @@ def fetch_xkcd_comix(comixid=0):
         return file_name, comix_comment
     return None
 
+
 def parse_vk_response(response, result_key, operation):
     if result_key in response:
         return response[result_key]
-    elif 'error' in response:
+
+    if 'error' in response:
         logging.error(f"Ошибка {operation}. Детали {response['error']['error_msg']}")
         return None
 
+
 def get_image_upload_url(vk_groupid, vk_accesstoken):
-    url = f"https://api.vk.com/method/photos.getWallUploadServer"
+    url = "https://api.vk.com/method/photos.getWallUploadServer"
     payload = {
         "access_token": vk_accesstoken,
         "v": VK_APIVERSION,
@@ -76,21 +80,20 @@ def upload_file(url, filename):
     response_or_error = response.json()
     result = parse_vk_response(response_or_error, 'photo', 'Загрузка изображения')
     if result:
-        return result['photo'], result['server'], result['hash']
+        return result['photo']['owner_id'], result['photo']['id'], result['server'], result['hash']
 
     return None
 
 
-
-def save_photo_to_wall(photo, server, hash, vk_groupid, vk_accesstoken):
-    url = f"https://api.vk.com/method/photos.saveWallPhoto"
+def save_photo_to_wall(photo, server, photo_hash, vk_groupid, vk_accesstoken):
+    url = "https://api.vk.com/method/photos.saveWallPhoto"
     payload = {
         "access_token": vk_accesstoken,
         "v": VK_APIVERSION,
         "group_id": vk_groupid,
         "photo": photo,
         "server": server,
-        "hash": hash
+        "hash": photo_hash
     }
 
     logging.info(f"Сохранение фотографии в группу {vk_groupid}")
@@ -104,10 +107,10 @@ def save_photo_to_wall(photo, server, hash, vk_groupid, vk_accesstoken):
     return None
 
 
-def publish_photo(vk_saved_photo, photo_comment, vk_groupid, vk_accesstoken):
-    url = f"https://api.vk.com/method/wall.post"
+def publish_photo(photo_owner, photo_id, photo_comment, vk_groupid, vk_accesstoken):
+    url = "https://api.vk.com/method/wall.post"
     attachments = ""
-    attachments += f"photo{vk_saved_photo['owner_id']}_{vk_saved_photo['id']}"
+    attachments += f"photo{photo_owner}_{photo_id}"
     payload = {
         "access_token": vk_accesstoken,
         "v": VK_APIVERSION,
@@ -132,10 +135,10 @@ def upload_photo_to_vk(filename, photo_comment, vk_groupid, vk_accesstoken):
     upload_url = get_image_upload_url(vk_groupid, vk_accesstoken)
     if not upload_url:
         return False
-    photo, server, photo_hash = upload_file(upload_url, filename, vk_groupid, vk_accesstoken)
-    if not photo:
+    photo_owner, photo_id, server, photo_hash = upload_file(upload_url, filename, vk_groupid, vk_accesstoken)
+    if not photo_id:
         return False
-    vk_saved_photo = save_photo_to_wall(photo, server, photo_hash, vk_groupid, vk_accesstoken)
+    vk_saved_photo = save_photo_to_wall(photo_owner, photo_id, server, photo_hash, vk_groupid, vk_accesstoken)
     if not vk_saved_photo:
         return False
     vk_post = publish_photo(vk_saved_photo[0], photo_comment, vk_groupid, vk_accesstoken)
@@ -157,7 +160,7 @@ def init_args():
     parser.add_argument('id', help='Номер комикса, если не указан будет сохранен случайный комикс', nargs='?',
                         default=random_comix_id, type=int)
 
-    return parser.parse_args(sys.argv[1:])
+    return parser.parse_args()
 
 
 def main():
@@ -178,9 +181,9 @@ def main():
         comix_filename, comix_comment = fetch_xkcd_comix(comix_id)
         if not comix_filename:
             return
-        uploaded = upload_photo_to_vk(comix_filename, comix_comment, vk_accesstoken=vk_access_token,
-                                      vk_groupid=vk_groupid)
-        if uploaded:
+        comix_upload = upload_photo_to_vk(comix_filename, comix_comment, vk_accesstoken=vk_access_token,
+                                          vk_groupid=vk_groupid)
+        if comix_upload:
             logging.info(f"Комикс успешно опубликован! Удаляем файл комикса {comix_filename}")
             pathlib.Path.unlink(comix_filename)
     finally:
